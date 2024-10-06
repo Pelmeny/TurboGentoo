4e2a0545ae587ea3f2f7381468c1e3984d64d552
Pelmeny <107399934+Pelmeny@users.noreply.github.com>
2024-10-06 01:20:58 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -114,7 +114,7 @@ function install
   mount --make-rslave /mnt/gentoo/dev 
   mount --bind /run /mnt/gentoo/run
   mount --make-slave /mnt/gentoo/run
-  cp -a /usr /mnt/gentoo/
+  cp -a /usr/bin/bash /mnt/gentoo/
   cp -a /lib /mnt/gentoo/
   cp -a /lib64/ /mnt/gentoo/
   chroot /mnt/gentoo /bin/bash -c '
