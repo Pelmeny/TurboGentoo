4d0aa1964826e6cbace97137a77af0254f16d646
Pelmeny <107399934+Pelmeny@users.noreply.github.com>
2024-10-06 11:36:57 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -73,8 +73,6 @@ function makeconf
     cp ./make.conf /mnt/gentoo/etc/portage/
     echo
     echo "please chage this file to use on your PC"
-    profile=25
-    end=1
     read -n 1 -s -r -p "Press any key to continue"
     $editor /mnt/gentoo/etc/portage/make.conf
   
@@ -107,16 +105,7 @@ function install
 
   echo "chroot"
   cp ./tochroot.sh /mnt/gentoo/ 
-  mount --types proc /proc /mnt/gentoo/proc/
-  mount --rbind /sys /mnt/gentoo/sys
-  mount --make-rslave /mnt/gentoo/sys 
-  mount --rbind /dev /mnt/gentoo/dev 
-  mount --make-rslave /mnt/gentoo/dev 
-  mount --bind /run /mnt/gentoo/run
-  mount --make-slave /mnt/gentoo/run
-  mount -o /lib /mnt/gentoo/lib
-  mount -o /lib64 /mnt/gentoo/lib
-  chroot /mnt/gentoo /bin/bash -c '
+  arch-chroot /mnt/gentoo '
   source /etc/profile
   export PS1="(chroot) ${PS1}"
   sh ./tochroot.sh
